dfc8be0a82fbdc698e108707aafd1208304303e8
Vladislav Mantsevich <vladmanest@gmail.com>
2026-04-27 04:45:04 +0300

    Everithing commited

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.11-slim
+WORKDIR /app
+ENV APP_PORT=8080
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+COPY . .
+RUN mkdir -p /app/data
+EXPOSE ${APP_PORT}
+CMD ["python", "main.py"]
+
---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+# Папка для данных (внутри контейнера)
+DATA_DIR = "/app/data"
+DB_FILE = os.path.join(DATA_DIR, "genes.txt")
+
+# Создаем папку, если её нет
+os.makedirs(DATA_DIR, exist_ok=True)
+
+class Gene(BaseModel):
+    name: str = Field(
+        default="ABCD",
+        description="Название гена, который ходите сохранить в app/data/genes.txt"
+        )
+    description: str = Field(
+        default="",
+        description="Описание гена, который ходите сохранить в app/data/genes.txt"
+        )
+
+@app.get("/")
+def read_root():
+    return {"message": "API is running from Docker!"}
+
+@app.post("/genes/")
+def add_gene(gene: Gene):
+    with open(DB_FILE, "a") as f:
+        f.write(f"{gene.name}: {gene.description}\n")
+    return {"status": "saved", "gene": gene.name}
+
+@app.get("/genes/")
+def get_genes():
+    if not os.path.exists(DB_FILE):
+        return {"genes": []}
+    with open(DB_FILE, "r") as f:
+        lines = f.readlines()
+    return {"genes": [line.strip() for line in lines]}
+
+if __name__ == "__main__":
+    import uvicorn
+    # Берем порт из переменной окружения или ставим 8000 по умолчанию
+    port = int(os.getenv("APP_PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
